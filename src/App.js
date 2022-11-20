5a05a628378ea0e951ff26605f47d4aa6f2f1ea8
RonaldoWidiantoSiswoyo <ronaldowidianto123@gmail.com>
2022-11-20 21:06:38 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { getMovieList, searchMovie } from "./api";
+import { useEffect } from "react";
 
-function App() {
+const App = () => {
+  useEffect(() => {
+    getMovieList();
+  }, []);
+
+  const search = (q) => {
+    console.log({ q });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Dow Flix</h1>
+        <input
+          type="text"
+          placeholder="Cari film kesayangan anda.."
+          className="movie-search"
+          onChange={({ target }) => search(target.value)}
+        />
+        <div className="movie-container">
+          <div className="movie-wrapper">
+            <div className="movie-title">Contoh Title</div>
+            <img className="movie-image" src="" />
+            <div className="movie-date">11-12-2022</div>
+            <div className="movie-rate">8.9</div>
+          </div>
+        </div>
       </header>
     </div>
   );
-}
+};
 
 export default App;
